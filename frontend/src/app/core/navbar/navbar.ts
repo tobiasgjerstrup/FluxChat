6e9dd15a76c68d267327e878f4f370e984f18cfa
Tobias Gjerstrup <tobiasgjerstrup@gmail.com>
2026-02-12 18:28:20 +0100

    Added a modal

--- frontend/src/app/core/navbar/navbar.ts
+++ frontend/src/app/core/navbar/navbar.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Modal } from '../../shared/modal/modal';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [Modal],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  showNotificationsModal = false;
 
+  openNotificationsModal() {
+    this.showNotificationsModal = true;
+  }
+
+  closeNotificationsModal() {
+    this.showNotificationsModal = false;
+  }
 }
