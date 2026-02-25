442616f9e3df63ecc9fb2a335badaabe15c69f09
Tobias Gjerstrup <tobiasgjerstrup@gmail.com>
2026-02-25 19:07:29 +0100

    Add friend now lists all users

--- frontend/src/app/features/friends/add-friend/add-friend.ts
+++ frontend/src/app/features/friends/add-friend/add-friend.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Api } from '../../../core/api';
 
 @Component({
-  selector: 'app-add-friend',
-  imports: [],
-  templateUrl: './add-friend.html',
-  styleUrl: './add-friend.scss',
+    selector: 'app-add-friend',
+    imports: [],
+    templateUrl: './add-friend.html',
+    styleUrl: './add-friend.scss',
 })
 export class AddFriend {
+    constructor(private api: Api) {}
 
+    public users = signal<{ id: number; username: string }[]>([]);
+
+    ngOnInit() {
+        this.getUsers();
+    }
+
+    public async getUsers() {
+        this.users.set((await this.api.getUsers()).users);
+    }
+
+    public addFriend(userId: number) {}
 }
