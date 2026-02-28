ae1dc4e0b3a1606fbfc0f41de4d8e9ddfa8ab61f
Tobias Gjerstrup <tobiasgjerstrup@gmail.com>
2026-02-28 20:35:24 +0100

    Refactor HttpError class for consistent indentation

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -1,10 +1,11 @@
 export class HttpError extends Error {
-  httpCode: number;
+    httpCode: number;
 
-  constructor(message: string, httpCode: number) {
-    super(message);
-    this.name = 'HttpError';
-    this.httpCode = httpCode;
-    Error.captureStackTrace(this, this.constructor);
-  }
+    constructor(message: string, httpCode: number) {
+        super(message);
+        this.name = 'HttpError';
+        this.httpCode = httpCode;
+        Error.captureStackTrace(this, this.constructor);
+    }
 }
+// test
