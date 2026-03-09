14847b5b809e23809feee1e6fb940e27dc66690e
Tobias Gjerstrup <tobiasgjerstrup@gmail.com>
2026-03-09 17:05:41 +0100

    Made eslint VERY strict

--- backend/eslint.config.ts
+++ backend/eslint.config.ts
@@ -4,6 +4,24 @@ import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-    { files: ['**/*.{ts}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.node } },
-    tseslint.configs.recommended,
+    { ignores: ['eslint.config.ts', 'dist/**', '**/*.js'] },
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            globals: globals.node,
+            parserOptions: {
+                project: ['./tsconfig.eslint.json'],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    js.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    {
+        files: ['**/*.ts'],
+        rules: {
+            '@typescript-eslint/no-duplicate-type-constituents': 'error',
+        },
+    },
 ]);
